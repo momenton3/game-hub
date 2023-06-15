7f06fa7490ebf387b24aaa9cfc10754e780666d6
June <junnn2w@gmail.com>
2023-06-15 22:51:41 +0800

    Fetch the game (API correction)

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-    baseURL: 'https://api.rawg.io/api/games',
+    baseURL: 'https://api.rawg.io/api',
     params: {
         key: '83e117a0edf64adb9bbd3dda0d276908'
     }
