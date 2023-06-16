6dc320f5e3cea0559ade93064c15a125626573a0
June <junnn2w@gmail.com>
2023-06-16 17:18:22 +0800

    Get optimized images

--- src/components/CriticScore.tsx
+++ src/components/CriticScore.tsx
@@ -6,9 +6,9 @@ interface Props {
 
 const CriticScore = ( { score }: Props ) => { 
    let color = score > 75 ? 'green' : score > 60 ? 'yellow' : '';
-  return (
-    <Badge colorScheme={color} fontSize='14px' paddingX={2} borderRadius='4px'>{score}</Badge>
-  )
+   return (
+      <Badge colorScheme={color} fontSize='14px' paddingX={2} borderRadius='4px'>{score}</Badge>
+   )
 }
 
-export default CriticScore 
+export default CriticScore  
